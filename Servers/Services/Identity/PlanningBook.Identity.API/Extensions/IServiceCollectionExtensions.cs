946fd20fdecd3675d9360cdad1f095821e771151
Phat_Tran_Dev <phattrandev@gmail.com>
2024-08-28 08:50:59 +0700

    Update Identity Service

---
+++ Servers/Services/Identity/PlanningBook.Identity.API/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using PlanningBook.Domain;
+using PlanningBook.Domain.Interfaces;
+using PlanningBook.Identity.Application.Commands.Accounts;
+
+namespace PlanningBook.Identity.API.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection RegistryCommandQueryExecutor(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<ICommandExecutor, CommandExecutor>();
+            services.AddTransient<IQueryExecutor, QueryExecutor>();
+            return services;
+        }
+
+        public static IServiceCollection RegistryAccountModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICommandHandler<CreateAccountByUserNameCommand, Guid>, CreateAccountByUserNameCommandHandler>();
+
+            return services;
+        }
+    }
+}
---
+++ Servers/Services/Identity/PlanningBook.Identity.Application/Commands/Accounts/CreateAccountByUserNameCommand.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PlanningBook.Domain.Interfaces;
+using PlanningBook.Identity.Infrastructure;
+using PlanningBook.Identity.Infrastructure.Entities;
+using PlanningBook.Repository.EF;
+
+namespace PlanningBook.Identity.Application.Commands.Accounts
+{
+    #region Command Model
+    public sealed class CreateAccountByUserNameCommand : ICommand<Guid>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public CreateAccountByUserNameCommand(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+    #endregion Command Model
+
+    #region Command Handler
+    public sealed class CreateAccountByUserNameCommandHandler : ICommandHandler<CreateAccountByUserNameCommand, Guid>
+    {
+        private readonly PBIdentityDbContext _identityDbContext;
+        //private readonly IEFRepository<PBIdentityDbContext, Account, Guid> _accountRepository;
+        private readonly UserManager<Account> _accountManager;
+        //private readonly RoleManager<AccountRole> _roleManager;
+        public CreateAccountByUserNameCommandHandler(PBIdentityDbContext identityDbContext, UserManager<Account> accountManager)
+        {
+            _identityDbContext = identityDbContext;
+            _accountManager = accountManager;
+        }
+
+        public Task<Guid> HandleAsync(CreateAccountByUserNameCommand command, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion Command Handler
+}
