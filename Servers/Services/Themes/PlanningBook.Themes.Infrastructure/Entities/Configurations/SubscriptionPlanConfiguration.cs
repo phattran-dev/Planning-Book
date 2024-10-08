42294973a0f1f3dcc11fe9504a63d31155e7f52a
Phat_Tran_Dev <phattrandev@gmail.com>
2024-10-08 23:02:35 +0700

    Update code

--- Servers/Services/Themes/PlanningBook.Themes.Infrastructure/Entities/Configurations/SubscriptionPlanConfiguration.cs
+++ Servers/Services/Themes/PlanningBook.Themes.Infrastructure/Entities/Configurations/SubscriptionPlanConfiguration.cs
@@ -3,9 +3,9 @@ using PlanningBook.DBEngine;
 
 namespace PlanningBook.Themes.Infrastructure.Entities.Configurations
 {
-    public class SubscriptionPlanConfiguration : BaseRelationDbEntityTypeConfiguration<SubscriptionPlanConfiguration>
+    public class SubscriptionPlanConfiguration : BaseRelationDbEntityTypeConfiguration<SubscriptionPlan>
     {
-        public override void Configure(EntityTypeBuilder<SubscriptionPlanConfiguration> builder)
+        public override void Configure(EntityTypeBuilder<SubscriptionPlan> builder)
         {
             base.Configure(builder);
         }
