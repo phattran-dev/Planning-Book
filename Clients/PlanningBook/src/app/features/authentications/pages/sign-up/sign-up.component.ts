442b0d218be0ac9bb38614e3c8e855cbc51ddd2d
Phat_Tran_Dev <phattrandev@gmail.com>
2024-11-21 02:21:14 +0700

    Update

--- Clients/PlanningBook/src/app/features/authentications/pages/sign-up/sign-up.component.ts
+++ Clients/PlanningBook/src/app/features/authentications/pages/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../../../shared/services/authentications.service';
+import { SignUpRequest } from '../../../../shared/models/authentications.model';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
@@ -25,5 +27,33 @@ export class SignUpComponent {
 
   onClick() {
     console.log('test');
+  }
+
+  onSignUp() {
+    if (this.signUpForm.controls.password.value !== this.signUpForm.controls.confirmPassword.value) {
+      console.log('Sign Up Error 1');
+      return;
+    }
+
+    const signUpRequest: SignUpRequest = {
+      userName: this.signUpForm.controls.username.value ?? '',
+      password: this.signUpForm.controls.password.value ?? ''
+    };
+
+    this.#authenticationService.signUp(signUpRequest).subscribe(res => {
+      if (res.isSuccess && res.data) {
+        // localStorage.setItem('token', res.data?.token);
+        // localStorage.setItem('refresh-token', res.data?.refreshToken);
+        // localStorage.setItem('userId', res.data?.userId);
+        this.#router.navigateByUrl('/sign-in');
+      } else {
+        console.log('SignIn Error 2');
+      }
+    }, catchError(err => {
+      console.log('SignIn Error 3');
+      console.log(err);
+      return of();
+    }));
+
   }
 }
